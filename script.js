a613da6ecfb96b68e6722f8b6e231bef3fd49103
emines0 <emin.sefa1@gmail.com>
2022-10-01 21:38:53 +0200

    toogle navigation while clicking on link mob. version

--- script.js
+++ script.js
@@ -2,8 +2,15 @@
 //navigation
 const toggle = document.getElementById('toggle');
 const nav = document.getElementById('nav');
+const navLinks = document.querySelectorAll('.link')
 
-console.log('TOGGLE ' + toggle);
-console.log('NAV ' + nav);
+// console.log('TOGGLE ' + toggle);
+// console.log('NAV ' + nav);
 
 toggle.addEventListener('click', () => nav.classList.toggle('active'));
+
+if (document.documentElement.clientWidth <= 810) {
+    navLinks.forEach(link => link.addEventListener("click", function(e) {
+        nav.classList.toggle('active')
+    }));  
+}
